804fa2a59fe6aabb2291e2785ab8dec63d686f12
Nimay Gupta <nimaygupta18@gmail.com>
2021-03-26 15:32:40 +0530

    lab6 updated

--- Lab6-Neo4j/loaddata.py
+++ Lab6-Neo4j/loaddata.py
@@ -2,44 +2,51 @@ import csv
 
 file = open("loaddata.cypher", 'w')
 
+user = {}
+tweet = {}
+hashtag = {}
+
 with open("users.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
     for row in csvreader:
-        file.write('CREATE (:User {id: ' + row[0] + ', name: "' + row[1] + '"});\n')
+        user[row[0]] = row[1]
+        file.write('CREATE (:User {name: "' + row[1] + '"});\n')
 
 with open("tweets.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
     for row in csvreader:
-        file.write('CREATE (:Tweet {id: ' + row[0] + ', text: "' + row[1] + '"});\n')
+        tweet[row[0]] = row[1]
+        file.write('CREATE (:Tweet {text: "' + row[1] + '"});\n')
 
 with open("hashtags.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
     for row in csvreader:
-        file.write('CREATE (:Hashtag {id: ' + row[0] + ', tag: "' + row[1] + '"});\n')
+        hashtag[row[0]] = row[1]
+        file.write('CREATE (:Hashtag {tag: "' + row[1] + '"});\n')
 
 with open("follows.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
     for row in csvreader:
-        file.write('MATCH (a:User {id: ' + row[0] + '}), (b:User {id: ' + row[1] + '}) CREATE (a)-[:Follows]->(b);\n')
+        file.write('MATCH (a:User {name: "' + user[row[0]] + '"}), (b:User {name: "' + user[row[1]] + '"}) CREATE (a)-[:Follows]->(b);\n')
 
 with open("sent.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
     for row in csvreader:
-        file.write('MATCH (a:User {id: ' + row[0] + '}), (b:Tweet {id: ' + row[1] + '}) CREATE (a)-[:Sent]->(b);\n')
+        file.write('MATCH (a:User {name: "' + user[row[0]] + '"}), (b:Tweet {text: "' + tweet[row[1]] + '"}) CREATE (a)-[:Sent]->(b);\n')
 
 with open("mentions.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
     for row in csvreader:
-        file.write('MATCH (a:Tweet {id: ' + row[0] + '}), (b:User {id: ' + row[1] + '}) CREATE (a)-[:Mentions]->(b);\n')
+        file.write('MATCH (a:Tweet {text: "' + tweet[row[0]] + '"}), (b:User {name: "' + user[row[1]] + '"}) CREATE (a)-[:Mentions]->(b);\n')
 
 with open("contains.csv", 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
     for row in csvreader:
-        file.write('MATCH (a:Tweet {id: ' + row[0] + '}), (b:Hashtag {id: ' + row[1] + '}) CREATE (a)-[:Contains]->(b);\n')
+        file.write('MATCH (a:Tweet {text: "' + tweet[row[0]] + '"}), (b:Hashtag {tag: "' + hashtag[row[1]] + '"}) CREATE (a)-[:Contains]->(b);\n')
