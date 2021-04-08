504238e97d74b493c0ddeeaae7654c41129da69c
Nimay Gupta <49588368+nimay-gupta@users.noreply.github.com>
2021-04-09 01:20:54 +0530

    Update stockreturns.py

--- Lab7-Spark/stockreturns.py
+++ Lab7-Spark/stockreturns.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SQLContext
 from pyspark import SparkContext
-# other required imports here
-   
+
 if __name__ == "__main__":
     # create Spark context with necessary configuration
     spark = SparkContext("local", "Stock Returns")
@@ -10,9 +9,9 @@ if __name__ == "__main__":
     df = SQLContext(spark).read.option("header", True).csv("stock_prices.csv")
 
     # calculate daily percentage returns
-    df = df.withColumn("return", 100*(df["close"]-df["open"])/df["open"])
+    df = df.withColumn("return", ((df["close"]-df["open"])/df["open"])*100)
 
-    # average on company name
+    # average on date
     df = df.groupBy("date").avg().alias("avg_return")
 
     # save the average returns to output
